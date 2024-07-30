62c7aeeb22dc2fbd7beedd88533340e282e6062a
zzuhann <zzuhanlin@gmail.com>
2024-07-30 23:48:23 +0800

    feat: add get specific id person

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.get("/info", (req, res) => {
   res.send(
     `<p>Phone book has info for ${
